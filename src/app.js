63f559f0fd66cc561636dc154e41c15a78f86f36
Migel <dmigel@yandex.ru>
2023-11-24 13:35:17 +0300

    done module 1 for tests

--- src/app.js
+++ src/app.js
@@ -1,20 +1,8 @@
 import '/src/app.scss'
 
+import '/src/components/input/styles.scss'
+import '/src/components/button/styles.scss'
 
-console.log('123')
 
-document.addEventListener("DOMContentLoaded", function () {
-  const openProfileUploadImageModal = document.querySelector('.profile__upload-btn')
-  const modal = document.querySelector('.modal')
-  openProfileUploadImageModal.addEventListener('click', function () {
-    modal.style.opacity = 1
-    modal.style.pointerEvents = 'auto'
-  })
-  document.addEventListener('click', function (event) {
-    if (event.target === modal) {
-      modal.removeAttribute('style');
-    }
-  })
-})
 
 
