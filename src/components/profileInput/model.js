ebd0a04a5b12d49b088f6918c0e84cf917533770
Migel <dmigel@yandex.ru>
2023-12-01 15:09:02 +0300

    test model profile inpup

---
+++ src/components/profileInput/model.js
@@ -0,0 +1,9 @@
+class profileInputModel {
+  constructor(placeholder, type, name, value, isActive) {
+    this.placeholder = placeholder
+    this.formType = type
+    this.formName = name
+    this.value = value
+    this.status = isActive
+  }
+}
